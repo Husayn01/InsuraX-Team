9fab2a854cea7f1612a860f6ffe5e0a800e5f7fd
Husayn01 <hussainiahmed222@gmail.com>
2025-07-23 08:13:50 +0100

    moved files

--- temp/index.js
+++
@@ -1,6 +0,0 @@
-// Export all layout components
-export { DashboardLayout } from './DashboardLayout'
-export { PageHeader } from './PageHeader'
-export { StatsCard } from './StatsCard'
-export { EmptyLayout } from './EmptyLayout'
-export { AuthLayout } from './AuthLayout'
